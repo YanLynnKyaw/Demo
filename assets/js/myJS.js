d1caa5a8361cb222a987c85533661c6f248c023d
KyawG <yanlinnkyaw.new@gmail.com>
2024-07-03 22:29:12 +0630

    fix smth and added our service

--- assets/js/myJS.js
+++ assets/js/myJS.js
@@ -10,3 +10,45 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+
+// document.querySelectorAll('.scroll-link').forEach(anchor => {
+//   anchor.addEventListener('click', function(e) {
+//     e.preventDefault();
+//     const target = document.querySelector(this.getAttribute('href'));
+//     window.scrollTo({
+//       top: target.offsetTop,
+//       behavior: 'smooth'
+//     });
+//   });
+// });
+
+// document.querySelectorAll('.scroll-link').forEach(anchor => {
+//   anchor.addEventListener('click', function(e) {
+//     e.preventDefault();
+//     const target = document.querySelector(this.getAttribute('href'));
+//     const duration = 1000; // Duration in milliseconds
+
+//     const start = window.pageYOffset;
+//     const end = target.getBoundingClientRect().top + window.pageYOffset;
+//     const distance = end - start;
+//     let startTime = null;
+
+//     function animation(currentTime) {
+//       if (startTime === null) startTime = currentTime;
+//       const timeElapsed = currentTime - startTime;
+//       const run = ease(timeElapsed, start, distance, duration);
+//       window.scrollTo(0, run);
+//       if (timeElapsed < duration) requestAnimationFrame(animation);
+//     }
+
+//     function ease(t, b, c, d) {
+//       t /= d / 2;
+//       if (t < 1) return c / 2 * t * t + b;
+//       t--;
+//       return -c / 2 * (t * (t - 2) - 1) + b;
+//     }
+
+//     requestAnimationFrame(animation);
+//   });
+// });
